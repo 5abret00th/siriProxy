ea97a0334869a7cc5e527b15f6e394544f44512b
5abret00th <dev@5abret00th.com>
2011-11-25 19:45:37 +0100

    debug

--- plugins/soccerScores/soccerScores.rb
+++ plugins/soccerScores/soccerScores.rb
@@ -59,7 +59,7 @@ class SoccerScores < SiriPlugin
       else
         response = "The score for the " + userTeam + " game is: " + @firstTeamName + " (" + @firstTeamScore + "), " + @secondTeamName + " (" + @secondTeamScore + ")"
 			end
-			connection.inject_object_to_output_stream(generate_siri_utterance(connection.lastRefId, response))
+			connection.inject_object_to_output_stream(generate_siri_utterance(connection.lastRefId, "123response"))
     }
     connection.inject_object_to_output_stream(generate_siri_utterance(connection.lastRefId, "response"))
 		return "Checking on tonight's hockey games "+ userTeam
