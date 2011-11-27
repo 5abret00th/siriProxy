c9c61f545b032c98e5cb18ffd608f88bd6ec557b
5abret00th <dev@5abret00th.com>
2011-11-27 12:27:04 +0100

    test

--- todo/hockey/hockey.rb
+++ todo/hockey/hockey.rb
@@ -76,7 +76,7 @@ class SiriHockeyScores < SiriPlugin
       #}
 
                   #wsdl
-      response = [soap.request(:get_match_by_match_id=>"9998")]
+      response = soap.request(:get_match_by_match_id=>"9998")
       #response = soap.request :get_avail_sports
 
       puts "testtest"
