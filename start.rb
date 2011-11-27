510e9ad442464b2e3b9a6a214db4a1a2a3175f75
5abret00th <dev@5abret00th.com>
2011-11-27 15:35:45 +0100

    test

--- start.rb
+++ start.rb
@@ -3,8 +3,8 @@ $LOAD_PATH << File.dirname(__FILE__)
 $KCODE='u' #setting KCODE to unicode for Ruby 1.8
 
 #require 'plugins/testproxy/testproxy'
-#require 'plugins/soccerScores/soccerScores'
-require 'todo/hockey/hockey'
+require 'plugins/soccerScores/soccerScores'
+#require 'todo/hockey/hockey'
 # require 'plugins/thermostat/siriThermostat'
 # require 'plugins/eliza/eliza'
 # require 'plugins/twitter/siriTweet'
@@ -14,7 +14,7 @@ require 'siriProxy'
 
 #Also try Eliza -- though it should really not be run "before" anything else.
 #Also try Twitter -- must first configure keys in siriTweet.rb
-PLUGINS = [SiriHockeyScores]
+PLUGINS = [SoccerScores]
 
 proxy = SiriProxy.new(PLUGINS)
 
