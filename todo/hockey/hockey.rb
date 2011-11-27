8f061da127a5b2fd00795fc9219fd452d842d785
5abret00th <dev@5abret00th.com>
2011-11-27 14:34:01 +0100

    test

--- todo/hockey/hockey.rb
+++ todo/hockey/hockey.rb
@@ -81,8 +81,7 @@ class SiriHockeyScores < SiriPlugin
       @soap = SOAP::WSDLDriverFactory.new(@WSDL_URL).create_rpc_driver
       puts "Lade alle Sachen initial in den Cache"
       spieltag = @soap.GetCurrentGroupOrderID(:leagueShortcut=>"bl1")
-      puts spieltag
-      response = @soap.GetMatchdataByGroupLeagueSaison(:groupOrderID=>"1",:leagueShortcut=>spieltag,:leagueSaison=>"2011")
+      response = @soap.GetMatchdataByGroupLeagueSaison(:groupOrderID=>"1",:leagueShortcut=>spieltag.GetCurrentGroupOderIRResult,:leagueSaison=>"2011")
       response.getMatchdataByGroupLeagueSaisonResult.matchdata.each{|item|
         #if item.matchID == "3"
         puts item.nameTeam1
