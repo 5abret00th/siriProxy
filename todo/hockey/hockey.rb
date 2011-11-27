f63ddbfee08a2f86090e26608218f14415c1c273
5abret00th <dev@5abret00th.com>
2011-11-27 14:43:17 +0100

    test

--- todo/hockey/hockey.rb
+++ todo/hockey/hockey.rb
@@ -82,7 +82,8 @@ class SiriHockeyScores < SiriPlugin
       puts "Lade alle Sachen initial in den Cache"
       spieltag = @soap.GetCurrentGroupOrderID(:leagueShortcut=>"bl1")
       puts "groupID geladen"
-      response = @soap.GetMatchdataByGroupLeagueSaison(:groupOrderID=>"1",:leagueShortcut=>spieltag.GetCurrentGroupOrderIDResponse.GetCurrentGroupOrderIDResult,:leagueSaison=>"2011")
+      puts spieltag.GetCurrentGroupOrderIDResult
+      response = @soap.GetMatchdataByGroupLeagueSaison(:groupOrderID=>"1",:leagueShortcut=>"bl1",:leagueSaison=>"2011")
       response.getMatchdataByGroupLeagueSaisonResult.matchdata.each{|item|
         #if item.matchID == "3"
         puts item.nameTeam1
