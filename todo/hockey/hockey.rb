bfcc720aba932ab2c90ab8f25bc1d58f6fc7208a
5abret00th <dev@5abret00th.com>
2011-11-27 13:36:17 +0100

    ytest

--- todo/hockey/hockey.rb
+++ todo/hockey/hockey.rb
@@ -79,7 +79,7 @@ class SiriHockeyScores < SiriPlugin
                   #wsdl
       #@response =  @soap.request(:get_match_by_match_id=>"9998")
       @response = @soap.request :get_avail_sports
-      @response.GetAvailSportsResponse.sport.each {|test|
+      @response.getavailsportsresponse.sport.each {|test|
          puts test.sportsName
         }
         #@soap.version = 2
